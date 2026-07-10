8af3d45873c4940116cbf73cb535e3b010a900d7
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-10 23:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices = element.querySelectorAll(".price");
+	let total = 0;
+	prices.forEach((price)=>{
+		total +=price;
+	} )
+	const tr = document.createElement("tr");
+	tr.innerHTML = `<td>${total}</td>`
 };
 
 getSumBtn.addEventListener("click", getSum);
